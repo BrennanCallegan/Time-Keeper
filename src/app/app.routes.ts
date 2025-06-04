2c128a10a3a77ed019dfdb5092485a79806b53ad
BrennanCallegan <programmerperegrine@gmail.com>
2025-06-04 13:30:14 -0500

    TK-4 Created URL route for clock and defined component logic.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Pomodoro } from './pomodoro/pomodoro';
 import { NgModule } from '@angular/core';
 import { Timer } from './timer/timer';
 import { Stopwatch } from './stopwatch/stopwatch';
+import { Clock } from './clock/clock';
 
 //Add new components here
 export const routes: Routes = [
     {path: '', component: Pomodoro},
     {path: 'timer', component: Timer},
     {path: 'stopwatch', component: Stopwatch},
+    {path: 'clock', component: Clock}
 ];
 
 @NgModule({
--- src/app/clock/clock.ts
+++ src/app/clock/clock.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-clock',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './clock.css'
 })
 export class Clock {
+  date: string | Date | undefined
+  dateTime: string | undefined;
 
+  constructor() {
+    setInterval(() => {
+      this.getTime()
+    }, 100);
+  }
+
+  getTime() {
+    this.date = new Date();
+    const time = new Date();
+
+    this.dateTime = 
+      time.getHours() +
+      ":"
+      time.getMinutes() +
+      ":"
+      time.getSeconds();
+
+      this.date = formatDate(time, 'hh:mm:ss a', 'en-US');
+
+  }
 }
